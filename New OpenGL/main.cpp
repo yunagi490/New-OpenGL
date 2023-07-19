4eb6d166adc23e6da5ce774499f04155999e0620
yunagi490 <a23.2549tan@gmail.com>
2023-07-19 19:40:12 +0900

    Initial Commit

---
+++ New OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  New OpenGL
+//
+//  Created by 夕凪時雨 on 2023/07/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
